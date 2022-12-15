4f6faf36fd70b7ffab9cbc280d60cc5bf1b7e214
Adam Novak <adnosk@gmail.com>
2022-12-16 00:58:51 +0100

    Add DOM selectors and basic CSS

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
 const container = document.getElementById('container');
+const flexGrid = document.createElement('div');
+const box = document.createElement('div');
+
+flexGrid.setAttribute('id','flex-grid');
+flexGrid.innerHTML = 'test';
+container.appendChild(flexGrid);
