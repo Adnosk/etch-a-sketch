6e7649e78a8cbac57b6c735ecb2f221759e45e09
Adam Novak <adnosk@gmail.com>
2022-12-16 03:59:15 +0100

    Fix indentation

--- game.js
+++ game.js
@@ -1,9 +1,34 @@
 const container = document.getElementById('container');
+
 const playGridSection = document.createElement('div');
-const box = document.createElement('div');
 
-playGridSection.setAttribute('id','game');
-box.setAttribute('class', 'box');
-
+playGridSection.setAttribute('id', 'game');
 container.appendChild(playGridSection);
-playGridSection.appendChild(box);
+
+// create the grid
+function gridMaker(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement('div');
+            div.className = 'box';
+            playGridSection.appendChild(div);
+        }
+
+    }
+}
+
+// change the background color
+function changeColor(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+// default grid size
+let gridSize = 16;
+
+gridMaker(gridSize);
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', changeColor)
+});
